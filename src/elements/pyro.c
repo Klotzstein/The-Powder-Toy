dae477e8493a72e639e2814981d9925564d91aaf
Klotzstein Stein <legendarydot@googlemail.com>
2011-05-29 11:06:05 +0200

    Fixed Explosives bug

--- src/elements/pyro.c
+++ src/elements/pyro.c
@@ -9,7 +9,7 @@ int update_PYRO(UPDATE_FUNC_ARGS) {
 		parts[i].life = 0;
 	}
 
-	if(t==PT_FIRE && t==PT_EMBE && parts[i].life <=1)
+	if(t==PT_FIRE  && parts[i].life <=1)
 
 	{
 		if (parts[i].tmp==3){
@@ -39,20 +39,7 @@ int update_PYRO(UPDATE_FUNC_ARGS) {
 		}
 	}
 
-	    if(t==PT_EMBE && parts[i].life <=1)
-	{
-		if (parts[i].tmp==3){
-			t = PT_DSTW;
-			part_change_type(i,x,y,t);
-			parts[i].life = 0;
-		}
-		else if (parts[i].temp<625)
-		{
-			t = PT_SMKE;
-			part_change_type(i,x,y,t);
-			parts[i].life = rand()%20+250;
-		}
-	}
+
 
 	for (rx=-2; rx<3; rx++)
 		for (ry=-2; ry<3; ry++)
@@ -70,15 +57,15 @@ int update_PYRO(UPDATE_FUNC_ARGS) {
                     (rt!=PT_SPNG || parts[r>>8].life==0) &&
                     ptypes[rt].flammable && (ptypes[rt].flammable + (int)(pv[(y+ry)/CELL][(x+rx)/CELL]*10.0f))>(rand()%1000))
 				{
-					part_change_type(r>>8,x+rx,y+ry,PT_FIRE || PT_EMBE);
+					part_change_type(r>>8,x+rx,y+ry,PT_FIRE);
 					parts[r>>8].temp = restrict_flt(ptypes[PT_FIRE].heat + (ptypes[rt].flammable/2), MIN_TEMP, MAX_TEMP);
 					parts[r>>8].life = rand()%80+180;
 					parts[r>>8].tmp = parts[r>>8].ctype = 0;
 					if (ptypes[rt].explosive)
 						pv[y/CELL][x/CELL] += 0.25f * CFDS;
 				}
-                if (parts[i].type==PT_LAVA && t==PT_EMBE){
-                    if (parts[r>>8].type==PT_LAVA && t==PT_EMBE){
+                if (parts[i].type==PT_LAVA){
+                    if (parts[r>>8].type==PT_LAVA){
                         if (parts[i].ctype==PT_TIN){
                             if (parts[r>>8].ctype==PT_COPR){
                                 parts[i].type = PT_BRNZ;
@@ -112,14 +99,14 @@ int update_legacy_PYRO(UPDATE_FUNC_ARGS) {
 				rt = r&0xFF;
 				lpv = (int)pv[(y+ry)/CELL][(x+rx)/CELL];
 				if (lpv < 1) lpv = 1;
-				if (t!=PT_SPRK && ptypes[rt].meltable  && ((rt!=PT_RBDM && rt!=PT_LRBD) || t!=PT_SPRK) && ((t!=PT_FIRE&&t!=PT_PLSM&&t!=PT_BFLM&&t!=PT_EMBE) || (rt!=PT_METL && rt!=PT_IRON && rt!=PT_ETRD && rt!=PT_PSCN && rt!=PT_NSCN && rt!=PT_NTCT && rt!=PT_PTCT && rt!=PT_BMTL && rt!=PT_BRMT && rt!=PT_SALT && rt!=PT_INWR)) &&
+				if (t!=PT_SPRK && ptypes[rt].meltable  && ((rt!=PT_RBDM && rt!=PT_LRBD) || t!=PT_SPRK) && ((t!=PT_FIRE&&t!=PT_PLSM&&t!=PT_BFLM) || (rt!=PT_METL && rt!=PT_IRON && rt!=PT_ETRD && rt!=PT_PSCN && rt!=PT_NSCN && rt!=PT_NTCT && rt!=PT_PTCT && rt!=PT_BMTL && rt!=PT_BRMT && rt!=PT_SALT && rt!=PT_INWR)) &&
                     ptypes[rt].meltable*lpv>(rand()%1000))
 				{
-					if (t!=PT_LAVA && t!=PT_EMBE || parts[i].life>0)
+					if (t!=PT_LAVA || parts[i].life>0)
 					{
 						parts[r>>8].ctype = (rt==PT_BRMT)?PT_BMTL:parts[r>>8].type;
 						parts[r>>8].ctype = (parts[r>>8].ctype==PT_SAND)?PT_GLAS:parts[r>>8].ctype;
-						part_change_type(r>>8,x+rx,y+ry,PT_LAVA && PT_EMBE);
+						part_change_type(r>>8,x+rx,y+ry,PT_LAVA);
 						parts[r>>8].life = rand()%120+240;
 					}
 					else
@@ -140,7 +127,7 @@ int update_legacy_PYRO(UPDATE_FUNC_ARGS) {
 						return 1;
 					}
 
-					if (t==PT_LAVA && t==PT_EMBE)
+					if (t==PT_LAVA)
 					{
 						parts[i].life = 0;
 						t = parts[i].type = PT_STNE;
@@ -156,7 +143,7 @@ int update_legacy_PYRO(UPDATE_FUNC_ARGS) {
 						return 1;
 					}
 
-					if (t==PT_LAVA && t==PT_EMBE)
+					if (t==PT_LAVA)
 					{
 						parts[i].life = 0;
 						t = parts[i].type = (parts[i].ctype)?parts[i].ctype:PT_STNE;
